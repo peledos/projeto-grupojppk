a0ca30f2b0499500702cd5159e5e4d617e4ec4f9
Pedro Rafael <pedro_r_santiago@estudante.sesisenai.org.br>
2025-02-27 08:52:58 -0300

    Realizado testes e corrigido problemas que surgiram ao juntar as branchs

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import Cilindro from "./model/Cilindro.js"
 import Cone from "./model/Cone.js"
 document.getElementById("calcularCilindro").addEventListener("click", ()=>{
-    let cilindroCalcular = new Cilindro(document.getElementById("raio_cilindro"),document.getElementById("altura_cilindro"))
-    document.getElementById("resCilindro").innerHTML = `Área Total = ${cilindroCalcular.calcularAreaTotal()} <br> Volume = ${cilindroCalcular.calcularVolume()}`
+    let cilindroCalcular = new Cilindro(document.getElementById("raio_cilindro").value,document.getElementById("altura_cilindro").value)
+    document.getElementById("resCilindro").innerHTML = `Área Total = ${cilindroCalcular.calcularAreaTotal().toFixed(2)} <br> Volume = ${cilindroCalcular.calcularVolume().toFixed(2)}`
 })
 document.getElementById("calcularCone").addEventListener("click", ()=>{
-    let coneCalcular = new Cone(document.getElementById("raio_cone"),document.getElementById("altura_cone"))
-    document.getElementById("resCone").innerHTML = `Área Total = ${coneCalcular.calcularAreaTotal()} <br> Volume = ${coneCalcular.calcularVolume()}`
+    let coneCalcular = new Cone(document.getElementById("raio_cone").value,document.getElementById("altura_cone").value)
+    document.getElementById("resCone").innerHTML = `Área Total = ${coneCalcular.calcularAreaTotal().toFixed(2)} <br> Volume = ${coneCalcular.calcularVolume().toFixed(2)}`
 })
